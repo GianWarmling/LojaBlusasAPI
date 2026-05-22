353b3a0af11010bb7a547f106432b2193b43c442
Gian Warmling <130003869+GianWarmling@users.noreply.github.com>
2026-05-21 21:08:48 -0300

    Adicionado imagem na classe Product

--- estudandoChat/LojaBlusasAPI/LojaBlusasAPI/Migrations/20260522000600_AddImage.Designer.cs
+++ estudandoChat/LojaBlusasAPI/LojaBlusasAPI/Migrations/20260522000600_AddImage.Designer.cs
@@ -3,6 +3,7 @@ using LojaBlusasAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LojaBlusasAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260522000600_AddImage")]
+    partial class AddImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
